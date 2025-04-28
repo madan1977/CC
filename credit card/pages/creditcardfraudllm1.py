26e60d81faf4d9a4e4a4f3707352b7a462a0589e
madan1977 <madan77@gmail.com>
2025-04-28 19:01:06 +0530

    Update creditcardfraudllm1.py

--- credit card/pages/creditcardfraudllm1.py
+++ credit card/pages/creditcardfraudllm1.py
@@ -1,7 +1,7 @@
 import timeit
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
+#import matplotlib.pyplot as plt
+#import plotly.express as px
 import numpy as np
 import warnings
 import pandas as pd
