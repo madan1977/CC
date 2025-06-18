1601ef98111790a25f4f49769b2b3fe4dd770e83
madan1977 <madan77@gmail.com>
2025-06-19 05:08:57 +0530

    Update Manual_Inference_SelfLearning.py

--- credit card/pages/Manual_Inference_SelfLearning.py
+++ credit card/pages/Manual_Inference_SelfLearning.py
@@ -16,7 +16,7 @@ def MISL():
     st.title("🧠 Manual Inference with Auto Self-Learning")
 
     # === CONFIG ===
-    DATA_PATH = r"backend_live_data.csv"
+    DATA_PATH = r"credit card/pages/backend_live_data.csv"
     TARGET_COLUMN = "Fraudulent"
     SELF_LEARN_FILE = "data/manual_self_learned.csv"
     MODEL_DIR = "models"
