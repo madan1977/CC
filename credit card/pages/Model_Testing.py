80cabec912319a3143cb132b4f93d92028439aac
madan1977 <madan77@gmail.com>
2025-06-19 17:31:18 +0530

    Update Model_Testing.py

--- credit card/pages/Model_Testing.py
+++ credit card/pages/Model_Testing.py
@@ -48,7 +48,7 @@ def model_testing_app():
     from sklearn.model_selection import train_test_split
     X = df.drop(columns=[target_column])
     y = df[target_column]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
 
 
     #trad_model_file = st.file_uploader("Upload Traditional Model (.pkl)", type=["pkl"])
