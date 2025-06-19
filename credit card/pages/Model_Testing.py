3c69caaac7c1781a564aed74ea3a9a6a6812c997
madan1977 <madan77@gmail.com>
2025-06-19 09:52:10 +0530

    Update Model_Testing.py

--- credit card/pages/Model_Testing.py
+++ credit card/pages/Model_Testing.py
@@ -18,9 +18,9 @@ def model_testing_app():
         X_test = test_df.drop(columns=[target_col])
         y_test = test_df[target_col]
 
-        trad_model_file = "pages/classical_model.pkl" 
+        trad_model_file = "credit card/pages/classical_model.pkl" 
                 #trad_model_file = st.file_uploader("Upload Traditional Model (.pkl)", type=["pkl"])
-        bilstm_model_file = "pages/bilstm_model.h5" 
+        bilstm_model_file = "credit card/pages/bilstm_model.h5" 
         if trad_model_file and bilstm_model_file:
             # Load traditional model
             with open(trad_model_file, "rb") as f:
