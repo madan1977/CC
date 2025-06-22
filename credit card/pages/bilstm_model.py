92e24a4b283cf817ceafb94bb595167cb8e6a1c0
madan1977 <madan77@gmail.com>
2025-06-22 16:10:22 +0530

    Update bilstm_model.py

--- credit card/pages/bilstm_model.py
+++ credit card/pages/bilstm_model.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense, Bidirectional, Dropout, Layer, Permute, Multiply, Lambda, Softmax
+from tensorflow.keras.layers import Input, LSTM, Dense, Bidirectional, Dropout, Layer, Permute, Multiply, Lambda, Softmax,BatchNormalization
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.backend as K
+from tensorflow.keras.models import Sequential
+
+
 
 class AttentionLayer(Layer):
     """
@@ -35,34 +38,27 @@ class AttentionLayer(Layer):
     def compute_output_shape(self, input_shape):
         return (input_shape[0], input_shape[-1])
 
-def build_bilstm_model(input_shape, output_dim):
-    """
-    BiLSTM with Attention for temporal sequence classification.
-    
-    Args:
-        input_shape (tuple): (sequence_length, num_features)
-        output_dim (int): number of output classes (e.g., 2 for fraud/non-fraud)
+def build_bilstm_model(input_shape, output_dim, loss_fn='binary_crossentropy'):
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.layers import LSTM, Bidirectional, Dense, Dropout, BatchNormalization
+    from tensorflow.keras.optimizers import Adam
+    from pages.bilstm_model import AttentionLayer
 
-    Returns:
-        Keras Model
-    """
-    inputs = Input(shape=input_shape)
+    model = Sequential()
+    model.add(Bidirectional(LSTM(64, return_sequences=True), input_shape=input_shape))
+    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
 
-    # BiLSTM layers
-    x = Bidirectional(LSTM(64, return_sequences=True))(inputs)
-    x = Dropout(0.3)(x)
+    model.add(Bidirectional(LSTM(32, return_sequences=True)))
+    model.add(AttentionLayer())
 
-    # Attention mechanism
-    attention_output = AttentionLayer()(x)
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.2))
 
-    # Dense layers
-    x = Dense(64, activation='relu')(attention_output)
-    x = Dropout(0.3)(x)
-    outputs = Dense(output_dim, activation='softmax')(x)
+    if output_dim == 1:
+        model.add(Dense(1, activation='sigmoid'))
+    else:
+        model.add(Dense(output_dim, activation='softmax'))
 
-    model = Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer=Adam(learning_rate=0.001),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    
+    model.compile(loss=loss_fn, optimizer=Adam(0.001), metrics=['accuracy'])
     return model
